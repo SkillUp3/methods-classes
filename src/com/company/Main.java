554200a40849c0eba5d7abd792b685f84d285265
Ian de Jesus <ian.dejesus.md@gmail.com>
2017-03-05 07:53:44 +0800

    Intiial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,68 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        int playerHealth = 100;
+        int attackDamage = 5;
+        int monsterHealth = 200;
+
+
+        monsterHealth -= playerAttack(attackDamage);
+        monsterHealth -= playerAttack(attackDamage);
+        monsterHealth -= playerAttack(attackDamage);
+        monsterHealth -= playerAttack(attackDamage);
+        monsterHealth -= playerAttack(attackDamage);
+        monsterHealth -= playerAttack(attackDamage, 4);
+
+        System.out.println("the health left of the monster is" + monsterHealth + " hp");
+
+    }
+
+    public static  int playerAttack(int attackDamage){
+        int finalAttack  = (int) (attackDamage * Math.random());
+        return finalAttack;
+
+    }
+
+    public static  int playerAttack(int attackDamage, int bonusDamage){
+        int finalAttack  = (int) (attackDamage * Math.random() + Math.random()*bonusDamage);
+        return finalAttack;
+
+    }
+
+    int testValue = 132;
+
+
+    // Create a method called calcFeetAndInchesToCentimeters
+    // It needs to have two parameters.
+    // feet is the first parameter, inches is the 2nd parameter
+    //
+    // You should validate that the first parameter feet is >= 0
+    // You should validate that the 2nd parameter inches is >=0 and <=12
+    // return -1 from the method if either of the above is not true
+    //
+    // If the parameters are valid, then calculate how many centimetres
+    // comprise the feet and inches passed to this method and return
+    // that value.
+    //
+    // Create a 2nd method of the same name but with only one parameter
+    // inches is the parameter
+    // validate that its >=0
+    // return -1 if it is not true
+    // But if its valid, then calculate how many feet are in the inches
+    // and then here is the tricky part
+    // call the other overloaded method passing the correct feet and inches
+    // calculated so that it can calculate correctly.
+    // hints: Use double for your number datatypes is probably a good idea
+    // 1 inch = 2.54cm  and one foot = 12 inches
+    // use the link I give you to confirm your code is calculating correctly.
+    // Calling another overloaded method just requires you to use the
+    // right number of parameters.
+
+}
+
+
+
